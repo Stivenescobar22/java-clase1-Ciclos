376ca3259c6fc335082f72b8de1ab18b5ff1b9ec
stiven escobar galeano <estibenes@gmail.com>
2020-03-08 12:43:16 -0500

    se implementa ciclos con ejercicios basicos

---
+++ src/main/Main.java
@@ -0,0 +1,47 @@
+package main;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String args[]){
+
+       Scanner teclado = new Scanner(System.in);
+
+       int i;
+       int j;//while
+       int k; //do..while
+
+
+        System.out.println("Ingrese valor para mostrar la tabla de multiplicar");
+        int valor = teclado.nextInt();
+
+        for( i=1; i<=10; i++){
+
+            System.out.println(valor + "x" + i + "=" + (valor*i));
+
+        }
+
+        System.out.println("");
+        System.out.println("");
+
+
+        j=1;
+        while(j <= 10){
+            System.out.println(valor + "x" + j + "=" + (valor*j));
+            j++;
+        }
+
+        System.out.println("");
+        System.out.println("");
+
+
+        k=1;
+        do {
+
+            System.out.println(valor + "x" + k + "=" + (valor*k));
+            k++;
+
+        }while(k <= 10);
+    }
+}
